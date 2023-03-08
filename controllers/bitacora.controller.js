577c3bc8072fc86b992e25bdb7c5c694aa4a9a43
Carlos Velasco <A01708634@tec.mx>
2023-03-07 18:49:13 -0600

    Se agregaron los datos correctos en la navbar y se le dio un poco de estilo al html de bitacora

--- controllers/bitacora.controller.js
+++ controllers/bitacora.controller.js
@@ -1,16 +1,16 @@
-const Registro = require("../models/bitacora.model");
+const Bitacora = require("../models/bitacora.model");
 
 exports.get_bitacora = (request, response, next) => {
-    response.render("bitacora/bitacora", {registros: Registro.fetchAll()});
+    response.render("bitacora/bitacora", {registros: Bitacora.fetchAll()});
 }
 
 exports.post_bitacora = (request,response,next) => {
-    const registro = new Registro({
+    const registro = new Bitacora({
         fecha: request.body.fecha,
         descr_sesion: request.body.descr_sesion,
         nivel_satisf: request.body.nivel_satisf,
         comentarios: request.body.comentarios,
     })
     registro.save();
-    response.redirect('/');
+    response.redirect('/home');
 }
