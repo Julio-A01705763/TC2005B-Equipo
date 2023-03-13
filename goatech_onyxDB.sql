ff6e996b46cb817d99bc62746acd861ce4c9378e
mafeermoreno <a01708653@tec.mx>
2023-03-12 21:10:39 -0600

    Hacer cambios

--- goatech_onyxDB.sql
+++ goatech_onyxDB.sql
@@ -7,10 +7,9 @@
 -- Server version: 10.6.11-MariaDB
 -- PHP Version: 7.4.19
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
+DROP DATABASE IF EXISTS goatech_onyxDB;
+CREATE DATABASE goatech_onyxDB;
+USE goatech_onyxDB;
 --
 -- Database: goatech_onyx
 --
