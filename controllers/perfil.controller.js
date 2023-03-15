f97e387f548e468859682927e42a99dc72bdb000
Carlos Velasco <A01708634@tec.mx>
2023-03-15 07:44:57 -0600

    Consulta a la base de datos para el perfil

--- controllers/perfil.controller.js
+++ controllers/perfil.controller.js
@@ -2,5 +2,17 @@ const Dieta = require('../models/dietas_favoritas.model');
 const Rutina = require('../models/rutinas_favoritas.model')
 
 exports.ver_perfil = (request, response, next) => {
-    response.render('perfil/perfil', {dieta : Dieta.fetchAll()[0] , rutina : Rutina.fetchAll()[0]});
+    let dietasRows = new Array;
+    let rutinasRows = new Array;
+    Dieta.fetchAll()
+    .then(([rows, fieldData]) => {
+        dietasRows.push(rows[0]);
+        Rutina.fetchAll()
+        .then(([rows, fieldData]) => {
+            rutinasRows.push(rows[0]);
+            response.render('perfil/perfil', {dieta: dietasRows[0], rutina: rutinasRows[0]})
+        })
+        .catch(error => console.log(error));
+    })
+    .catch(error => console.log(error));
 }
